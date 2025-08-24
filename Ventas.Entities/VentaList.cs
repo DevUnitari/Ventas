61e518ca2f183ab72d80f18ae7923be0cce33dc3
UnseR <UnseR@DESKTOP-4UUO683>
2025-08-24 17:56:57 -0400

    24/08/25

---
+++ Ventas.Entities/VentaList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ventas.Entities
+{
+    public class VentaList
+    {
+        public int numeroEmpleado { get; set; }
+        public string nombre { get; set; }
+        public int edad { get; set; }
+        public string cargo { get; set; }
+        public DateTime? fechaContrato { get; set; }
+        public double? cuota { get; set; }
+        public int? venta { get; set; }
+    }
+}
